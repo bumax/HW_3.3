2591cd843e61b7d74b8e984ce3c2e1ad9e098961
BuMax <max@butenko.in>
2022-12-04 22:08:02 +1000

    Exercise #23 completed

---
+++ ex23/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число N: ");
+var N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+    System.Console.WriteLine("Куб числа {0} равен {1}", i, Math.Pow(i, 3));
